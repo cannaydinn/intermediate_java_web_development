0d1e5908c4773da1e3a63790012e5e83a52aece3
Can Aydin <canaydinn6@gmail.com>
2023-11-18 16:58:05 +0300

    Forest Class Created

---
+++ java/AdventureGame/Forest.java
@@ -0,0 +1,9 @@
+package AdventureGame;
+public class Forest extends BattleLoc{
+
+    public Forest(Player player) {
+        super(player, "Orman", new Vampire(), "Odun", 3);
+        
+    }
+    
+}

622f0ba9e66960649abc28b4a0b15d5d3a034b2b
Can Aydin <canaydinn6@gmail.com>
2023-11-18 16:59:34 +0300

    NormalLoc Class Created

---
+++ java/AdventureGame/NormalLoc.java
@@ -0,0 +1,14 @@
+package AdventureGame;
+public class NormalLoc extends Location{
+
+    public NormalLoc(Player player, String name){
+        super(player, name);
+    }
+
+    @Override
+    public boolean onLocation() {
+        return true;
+    }
+    
+    
+}

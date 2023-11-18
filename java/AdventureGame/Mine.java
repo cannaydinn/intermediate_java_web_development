1a50999f9d856961107c5583c2900d1fcfcde498
Can Aydin <canaydinn6@gmail.com>
2023-11-18 16:59:20 +0300

    Mine Class Created

---
+++ java/AdventureGame/Mine.java
@@ -0,0 +1,9 @@
+package AdventureGame;
+public class Mine extends BattleLoc{
+
+    public Mine(Player player) {
+        super(player, "Maden", new Snake(), "Madenden rastgele düşecek item", 5);
+
+    }
+    
+}
